d978e1f91c14092342cfb4d3aa0009d7b4d61b78
Sepia <marc.jimenez.gimeno@gmail.com>
2025-04-09 23:30:43 +0200

    cleanup: Remove OLED luna animation

--- oled.c
+++ oled.c
@@ -1,7 +1,3 @@
-#ifdef LUNA_ENABLED
-#include "luna.c"
-#endif
-
 #ifdef BONGO_ENABLED
 #include "bongo.c"
 #endif
--- rules.mk
+++ rules.mk
@@ -7,9 +7,9 @@ EXTRAKEY_ENABLE = yes
 AUTO_SHIFT_ENABLE = no
 
 # START - OLED Enable
-BONGO_ENABLED = yes 
 OLED_ENABLE = yes
 WPM_ENABLE = yes
+BONGO_ENABLED = yes 
 OPT_DEFS += -DBONGO_ENABLED
 SRC += bongo.c
 SRC += ./lib/layer_state_reader.c
